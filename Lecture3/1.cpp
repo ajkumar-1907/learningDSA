b6fd9dddee44e52f01a0e020249ddf32cd0b15c9
ajkumar-1907 <seinanuj@gmail.com>
2025-03-20 20:19:04 +0530

    Lecture3

---
+++ Lecture3/1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+
+    //cout << "Value of n is : " << n << endl;
+
+    //if a is positive
+    if(a>0){
+        cout << " A is Positive" << endl;
+    }
+    else{
+        cout << "A is Negative" << endl;
+    }
+}
---
+++ Lecture3/2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // Your Code Here
+    int a,b;
+
+    //cin >> a >> b;
+    a = cin.get();
+
+    //cout << " value of a and b is " << a << "" << b << endl;
+
+    cout << " Value of a is : "<< a << endl; 
+}
