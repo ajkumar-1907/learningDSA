2cc55c56d2d83ac8590232c9e15d13b0e163e7b5
ajkumar-1907 <seinanuj@gmail.com>
2025-05-28 22:50:07 +0530

    LeetCode

---
+++ LeetCode/removeOccurences.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while (s.length() != 0 && s.find(part) < s.length()) {
+            s.erase(s.find(part), part.length());
+        }
+        return s;
+    }
+};
+
+int main() {
+    string s, part;
+    cout << "Enter the main string: ";
+    getline(cin, s);
+    cout << "Enter the substring to remove: ";
+    getline(cin, part);
+
+    Solution obj;
+    string result = obj.removeOccurrences(s, part);
+
+    cout << "String after removal: " << result << endl;
+    return 0;
+}
